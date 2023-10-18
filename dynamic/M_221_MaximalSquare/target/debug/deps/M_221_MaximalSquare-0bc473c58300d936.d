c0304151b8117ab6ab1e816594ed3ea6a8921933
Vusal Salmanov <salmanov.vh@gmail.com>
2023-10-18 11:51:49 +0400

    rm buidl files

--- dynamic/M_221_MaximalSquare/target/debug/deps/M_221_MaximalSquare-0bc473c58300d936.d
+++
@@ -1,5 +0,0 @@
-/Users/vusal/Projects/LeetCode/dynamic/M_221_MaximalSquare/target/debug/deps/M_221_MaximalSquare-0bc473c58300d936: src/lib.rs
-
-/Users/vusal/Projects/LeetCode/dynamic/M_221_MaximalSquare/target/debug/deps/M_221_MaximalSquare-0bc473c58300d936.d: src/lib.rs
-
-src/lib.rs:
